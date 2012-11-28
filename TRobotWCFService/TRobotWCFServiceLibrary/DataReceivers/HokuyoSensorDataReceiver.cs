c2463983284c728fa013671e049836ec1da12a07
Michal Gorski <mgorski@future-processing.com>
2012-11-28 22:25:56 +0100

    I added mock method for HokuyoSensor

--- TRobotWCFService/TRobotWCFServiceLibrary/DataReceivers/HokuyoSensorDataReceiver.cs
+++ TRobotWCFService/TRobotWCFServiceLibrary/DataReceivers/HokuyoSensorDataReceiver.cs
@@ -8,9 +8,23 @@ namespace TRobotWCFServiceLibrary.DataReceivers
 {
     class HokuyoSensorDataReceiver:IDataReceiver
     {
+        /// <summary> 
+        /// This is mock method. This method always returns distance value specyfied by equation: 'point'sNumber * 2'.
+        /// Range for this sensor is (2 - 400 cm).
+        /// Scan area is 240 degrees semicircle. Pitch angle is 0.36 degree and sensor outputs the distace measured at every point (683 steps).
+        /// The key for measured distance is 'distance0', 'distance1' ... 'distance682'.
+        /// </summary>
         public Data ReceiveData()
         {
-            throw new NotImplementedException();
+            Data data = new Data();
+
+            for (int i = 0; i < 683; i++)
+            {
+                String key = "distance" + i;
+                data.Dictionary.Add("key", i*2);
+            }
+
+            return data;
         }
     }
 }
