050672e964d2cbfdb4402faee35df18432eeef05
Michal Gorski <mgorski@future-processing.com>
2013-01-04 03:43:03 +0100

    Added new tests for DataReceiverFactory.

--- TRobotWCFService/UnitTestsProject/DataReceiverFactoryTest.cs
+++ TRobotWCFService/UnitTestsProject/DataReceiverFactoryTest.cs
@@ -93,5 +93,39 @@ namespace UnitTestsProject
             //  Then
             Assert.IsTrue(dataReceiver is TemperatureDataReceiver);
         }
+
+        /// <summary>
+        /// A test which tests if got DataReceiver is MobotSensorDataReceiver.
+        /// </summary>
+        [TestMethod()]
+        public void GetMobotSensorDataReceiverTest()
+        {
+            //  Given
+            DataReceiverFactory dataReceiverFactory = new DataReceiverFactory();
+            SelectedDevice dataReceiverType = SelectedDevice.Mobot;
+
+            //  When
+            IDataReceiver dataReceiver = dataReceiverFactory.GetDataReceiver(dataReceiverType);
+
+            //  Then
+            Assert.IsTrue(dataReceiver is MobotSensorDataReceiver);
+        }
+
+        /// <summary>
+        /// A test which tests if got DataReceiver is NullDataReceiver.
+        /// </summary>
+        [TestMethod()]
+        public void GetNullDataReceiverTest()
+        {
+            //  Given
+            DataReceiverFactory dataReceiverFactory = new DataReceiverFactory();
+            SelectedDevice dataReceiverType = SelectedDevice.Drive;
+
+            //  When
+            IDataReceiver dataReceiver = dataReceiverFactory.GetDataReceiver(dataReceiverType);
+
+            //  Then
+            Assert.IsTrue(dataReceiver is NullDataReceiver);
+        }
     }
 }
